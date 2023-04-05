2b3598052362634836c3f0ba157ee6ce3002e88c
YukiMatsunagadesu <toshiya1048.reon@icloud.com>
2023-04-05 17:26:44 +0900

    型

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,18 @@ const voo:string='hi';
 const fooe :boolean =false;
 const fe:null=null;
 const fed:undefined=undefined;
-const fooddd:"foo"="foo"
+const fooddd:"foo"="foo";
+const foooo:number[]=[1,2,3];
+const fooood:Array<number>=[1,2,3];
+const fooooge:(number|string)[]=[1,'d',3];
+const neogoe:[string,number]=["fe",3]
+const geg:any='j'
+const gegge:unknown='j' //anyよりはbetter
+function myVoidFunction(argument1: number, argument2: string): void {
+  console.log(`Argument 1 is ${argument1} and Argument 2 is ${argument2}.`);
+}
+myVoidFunction(123, "hello");
+
 
 export default function Home() {
   return (

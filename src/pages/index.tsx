22577b13835c9510b9e6fce3c319cf333ffc3be4
YukiMatsunagadesu <toshiya1048.reon@icloud.com>
2023-04-08 17:21:06 +0900

    tailwindcssとか諸々

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,14 @@ function addNumbers(num1: number, num2: number){
 }
 const result = addNumbers(2, 3);
 console.log(result); 
-const voo:string='hi';
+const voo:string='f'
 const fooe :boolean =false;
 const fe:null=null;
 const fed:undefined=undefined;
 const fooddd:"foo"="foo";
 const foooo:number[]=[1,2,3];
 const fooood:Array<number>=[1,2,3];
-const fooooge:(number|string)[]=[1,'d',3];
+const fooooge:(number|string)[]=[1,'d','f'];
 const neogoe:[string,number]=["fe",3]
 const geg:any='j'
 const gegge:unknown='j' //anyよりはbetter
@@ -24,12 +24,48 @@ function myVoidFunction(argument1: number, argument2: string): void {
 myVoidFunction(123, "hello");
 
 
+const person: Record<string, any> = {
+  firstName: 'grgohgrn',
+  lastName: 'Dogre',
+};
+
+const person2: {[key:string]:unknown} = {
+  firstName: 'grgohgrn',
+  lastName: 'Dogre',
+};
+
+type stringnumber= string|number; //型エイリアス
+
+enum Weekday {
+  Sunday = 0,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6
+}
+
+//intersectiontypes
+type foo={
+  a:number;
+  b:string;
+}
+type bar={
+  c:number;
+  d:string;
+}
+type foobar=foo & bar;
+
+
+
 export default function Home() {
   return (
     <>
-      <p>{foo}</p>
+      <div className='text-3xl'>{foo}あれ</div>
       <p>{fooo}</p>
       <p>{voo}</p>
+      <p className="text-red-500">ん</p>
     </>
   )
 }
